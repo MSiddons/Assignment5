ef154fce4db42e7246d6bd70b8a4e7c42aadd22b
Martin Siddons <gny17hvu@uea.ac.uk>
2017-11-27 23:15:50 +0000

    /m

--- Assignment5/Source.cpp
+++ Assignment5/Source.cpp
@@ -13,18 +13,18 @@ double convertC(double a)
 double convertF(double a)
 {
 
-	return ((a - 32) * 0.55556); // using a decimal approximation of 5/9 as we don't need more than 2dp of accuracy in everyday use.
-}								// (after all, you didn't say we needed to be 100% accurate!)
+	return ((a - 32) * 0.55556); // using a decimal approximation of 5/9 as we don't need more than 2dp of accuracy for everyday use.
+}								// (after all, you didn't say the output needed to be 100% accurate!)
 
 int main()
 {
 	double tempIn = 0, lastTemp = 1;
 	char degreesIn = 'a', lastDegrees = 'b';
-	while ((tempIn != lastTemp) || (degreesIn != lastDegrees))	// when both the tempreture and degrees values are the same as the the loop before last, exit.
-	{															// I'm not certain why, but && here worked as logical OR, and || works as logical AND.
+	while ((tempIn != lastTemp) || (degreesIn != lastDegrees))	// when both the tempreture and degrees values are the same as the loop before last, exit.
+	{															// I'm not certain why, but '&&' here acts as logical OR, and '||' acts as logical AND. Spooky.
 		lastTemp = tempIn;
 		lastDegrees = degreesIn;
-		cout << "Enter a temperature: " << setprecision(0); // setprecision(0) ensures the first part of the relevent cout will always 
+		cout << "Enter a temperature: " << fixed; // setprecision(0) ensures the first part of the relevent cout will always be shown correctly.
 		cin >> tempIn >> degreesIn;
 		if (degreesIn == 'c' || degreesIn == 'C')
 			cout << "When converted, " << tempIn << " degrees Celsius is " << fixed << setprecision(2) << convertC(tempIn) << " degrees Fahrenheit." << endl;
